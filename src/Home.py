e7af325807ac423da6889e00bb26ffd6e8322a6e
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-04-27 16:11:23 -0300

    Atualizando Projeto

--- src/Home.py
+++ src/Home.py
@@ -1,8 +1,9 @@
 import pandas as pd
-from pathlib import Path
 import streamlit as st
-import tempfile
 import plotly.express as px
+from jinja2 import FileSystemLoader,Environment
+from pathlib import Path
+from datetime import datetime
 
 def configurar_upload():
     page_config=st.set_page_config(page_title="FinDash",layout="wide",page_icon="💲") 
@@ -12,16 +13,12 @@ def configurar_upload():
     return upload_planilha
 
 def carregar_dataframe(arquivo) -> tuple[pd.DataFrame,str]:
-    with tempfile.TemporaryDirectory() as dir_temp:
-        dir_temp=Path(dir_temp) / arquivo.name
-        with open(dir_temp, mode="wb") as escritor:
-            escritor.write(arquivo.read())
-        if arquivo.name.endswith("xlsx"):
-            df=pd.read_excel(dir_temp)
-            tipo="xlsx"
-        if arquivo.name.endswith("csv"):
-            df=pd.read_csv(dir_temp)
-            tipo="csv"
+    if arquivo.name.endswith("xlsx"):
+        df=pd.read_excel(arquivo)
+        tipo="xlsx"
+    if arquivo.name.endswith("csv"):
+        df=pd.read_csv(arquivo)
+        tipo="csv"
     return df,tipo
 
 def selecionar_colunas(df) -> tuple[dict[str,str], bool]:
@@ -36,7 +33,7 @@ def selecionar_colunas(df) -> tuple[dict[str,str], bool]:
     if columns.button("Visualizar Dashboard"):
         st.session_state.colunas_selecionadas=True
     dict_colunas={"Data":coluna_data,
-                  "Categoria":coluna_categoria,
+                  "Categoria":coluna_categoria, 
                   "Tipo":coluna_tipo,
                   "Valor":coluna_valor}
     return dict_colunas,st.session_state.colunas_selecionadas
@@ -56,7 +53,7 @@ def formatar_colunas(df,colunas_dataframe) -> pd.DataFrame:
     df_formatado["Data"]=coluna_formatada
     return df_formatado
 
-def filtrar_df_formatado(df_formatado) -> pd.DataFrame:
+def filtrar_df_formatado(df_formatado) -> tuple[pd.DataFrame,str]:
     col1,col2=st.columns(2)
     ano=df_formatado["Data"].dt.year.unique()
     dict_meses={1:"Janeiro",2:"Fevereiro",3:"Março",4:"Abril",5:"Maio",6:"Junho",7:"Julho",8:"Agosto",9:"Setembro",10:"Outubro",11:"Novembro",12:"Dezembro"}
@@ -67,7 +64,7 @@ def filtrar_df_formatado(df_formatado) -> pd.DataFrame:
         filtro_mes=st.selectbox("Selecione o mês:",[dict_meses[mes] for mes in meses_disponiveis])
         df_filtrado=df_formatado.loc[(df_formatado["Data"].dt.year == filtro_ano) &
                                  (df_formatado["Data"].dt.month == [k for k, v in dict_meses.items() if v == filtro_mes][0])]
-    return df_filtrado
+    return df_filtrado,filtro_mes
 
 def graficos(df_filtrado) -> tuple[pd.DataFrame,pd.DataFrame]:
     col1,col2=st.columns(2)
@@ -91,9 +88,18 @@ def graficos(df_filtrado) -> tuple[pd.DataFrame,pd.DataFrame]:
         fig2=px.pie(df_receitas_mensais,names="Categoria",values="Valor",title="Distribuição das maiores Receitas",color="Categoria")
         fig2.update_traces(textinfo="percent+label")       
         col2.plotly_chart(fig2)
-    st.divider()
+    st.divider() 
     return df_receitas_e_despesas,df_receitas_mensais
 
+# def renderizando_template(filtro_mes):
+#     carregamento_pasta= FileSystemLoader(Path("templates"))
+#     env=Environment(loader=carregamento_pasta)
+#     template=env.get_template("template.jinja")
+    
+#     variaveis_template={"mês":filtro_mes,
+#                         "data": datetime.date(datetime.now()),
+#                         "hora": datetime.time(datetime.now()),
+#                         }
 def main():
     upload_planilha=configurar_upload()
     if upload_planilha is not None:
@@ -101,7 +107,7 @@ def main():
         dict_colunas,colunas_selecionadas=selecionar_colunas(df)
         if colunas_selecionadas==True:
             df_formatado=formatar_colunas(df,dict_colunas)
-            df_filtrado=filtrar_df_formatado(df_formatado)
+            df_filtrado,filtro_mes=filtrar_df_formatado(df_formatado)
             df_receitas_e_despesas,df_receitas_mensais=graficos(df_filtrado)
 if __name__ == "__main__":
     main()
