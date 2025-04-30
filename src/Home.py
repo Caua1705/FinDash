b08e52b213211bfeab158729b29168b00a6e884d
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-04-30 12:18:17 -0300

    Atualizando Projeto

--- src/Home.py
+++ src/Home.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-from jinja2 import FileSystemLoader,Environment
 from pathlib import Path
 from datetime import datetime
 import tempfile
 from openpyxl import load_workbook
+from openpyxl.styles import Alignment,Font,Border,Side,PatternFill
+from openpyxl.chart import BarChart, Reference
 
 def carregar_arquivo():
     st.set_page_config(page_title="FinDash",layout="wide",page_icon="💲") 
@@ -66,7 +67,7 @@ def filtrar_df_formatado(df_formatado) -> tuple[pd.DataFrame,str]:
         data_referencia=data_filtrada.strftime("%Y-%m")
     return df_filtrado,filtro_mes,data_referencia
 
-def gerar_dataframes_para_graficos(df_filtrado) ->tuple[pd.DataFrame,pd.DataFrame]:
+def filtrar_dataframes_para_graficos(df_filtrado) ->tuple[pd.DataFrame,pd.DataFrame]:
     df_receitas_despesas=df_filtrado.pivot_table(index="Categoria",
                                                        columns="Tipo",
                                                        values="Valor",
@@ -82,8 +83,8 @@ def gerar_dataframes_para_graficos(df_filtrado) ->tuple[pd.DataFrame,pd.DataFram
                                                            df_receitas_despesas["Receitas"].sum()
                                                            ,df_receitas_despesas["Despesas"].sum()]
     receitas_mensais=df_filtrado.loc[df_filtrado["Tipo"]=="receita"]
-    df_receitas_mensais=receitas_mensais.groupby("Categoria")["Valor"].sum().sort_values(ascending=False)
-    df_receitas_mensais=df_receitas_mensais.reset_index()
+    df_receitas_mensais=receitas_mensais.groupby("Categoria")["Valor"].sum().sort_values(ascending=False).reset_index()
+    df_receitas_mensais.loc[len(df_receitas_mensais)] = ["TOTAL",df_receitas_mensais["Valor"].sum()]
     return df_receitas_despesas,df_receitas_mensais
 
 def gerar_graficos(df_receitas_despesas,df_receitas_mensais) -> None:
@@ -95,32 +96,106 @@ def gerar_graficos(df_receitas_despesas,df_receitas_mensais) -> None:
         col1.plotly_chart(fig1)
     with col2:                  
         st.subheader("Categorias com maiores Receitas")
-        if len(df_receitas_mensais)>2:
-            df_receitas_mensais=df_receitas_mensais.loc[0:2]
-        fig2=px.pie(df_receitas_mensais,names="Categoria",values="Valor",title="Distribuição das maiores Receitas",color="Categoria")
+        df_receitas_mensais_sem_total=df_receitas_mensais.loc[df_receitas_mensais["Categoria"]!="TOTAL"]
+        if len(df_receitas_mensais_sem_total)>2:
+            df_receitas_mensais_sem_total=df_receitas_mensais_sem_total.loc[0:2]
+        fig2=px.pie(df_receitas_mensais_sem_total,names="Categoria",values="Valor",title="Distribuição das maiores Receitas",color="Categoria")
         fig2.update_traces(textinfo="percent+label")       
         col2.plotly_chart(fig2)
     st.divider() 
 
-def formatar_para_excel(df_receitas_despesas,df_receitas_mensais,data_referencia):
+def formatar_arquivo_excel(sheet):
+    if sheet.max_column<3:
+        tipo="incompleto"
+    else:
+        tipo="completo"
+    #Formatar Colunas:
+    for colunas in sheet.columns:
+        for celula in colunas:
+            if isinstance(celula.value,(int,float)):
+                celula.number_format='"R$"#,##0.00'
+            letras_colunas=celula.column_letter
+            sheet.column_dimensions[letras_colunas].width=20
+            celula.border=Border(left=Side(style="thin"),
+                                right=Side(style="thin"),
+                                top=Side(style="thin"),
+                                bottom=Side(style="thin"))
+            celula.font=Font(name="Calibri")
+    #Formatar Cabeçalho:
+    for celula in sheet[1]:
+        celula.alignment=Alignment(horizontal="center", vertical="center",)
+        celula.fill=PatternFill(start_color="004080",end_color="004080",fill_type="solid")
+        celula.font=Font(bold=True,color="FFFFFF")
+    #Formatar Totais:
+    for linha in sheet.iter_rows(min_row=sheet.max_row,max_row=sheet.max_row):
+        for celula in linha:
+            celula.font=Font(bold=True)
+            celula.fill=PatternFill(start_color="FFFFF99C",end_color="FFFFF99C",fill_type="solid")
+
+    grafico=BarChart()
+    categorias=Reference(sheet,min_col=1,max_col=1,min_row=2,max_row=sheet.max_row-1)
+
+    if tipo=="completo":
+        #Formatar Categoria:
+        for linha in sheet.iter_rows(min_row=2,max_row=sheet.max_row-1,min_col=1,max_col=1):
+            for celula in linha:
+                celula.fill=PatternFill(start_color="FFF4E6",end_color="FFF4E6",fill_type="solid")
+                celula.alignment=Alignment(horizontal="left")
+        #Formatar Receitas:
+        for linha in sheet.iter_rows(min_row=2,max_row=sheet.max_row-1,min_col=2,max_col=2):
+            for celula in linha:
+                celula.fill=PatternFill(start_color="A9D08E",end_color="A9D08E",fill_type="solid")
+                celula.alignment=Alignment(horizontal="right")
+        #Formatar Despesas:
+        for linha in sheet.iter_rows(min_row=2,max_row=sheet.max_row-1,min_col=3,max_col=3):
+            for celula in linha:
+                celula.fill=PatternFill(start_color="FF5757",end_color="FF5757",fill_type="solid")
+                celula.alignment=Alignment(horizontal="right")
+        dados=Reference(sheet,min_col=2,max_col=3,min_row=1,max_row=sheet.max_row-1)
+        grafico.title="Receitas e Despesas por Categoria"
+
+    else:
+        #Formatar Categoria:
+        for linha in sheet.iter_rows(min_row=2,max_row=sheet.max_row-1,min_col=1,max_col=1):
+            for celula in linha:
+                celula.fill=PatternFill(start_color="FFF4E6",end_color="FFF4E6",fill_type="solid")
+                celula.alignment=Alignment(horizontal="left")
+        #Formatar Valor Total:
+        for linha in sheet.iter_rows(min_row=2,max_row=sheet.max_row-1,min_col=2,max_col=2):
+            for celula in linha:
+                celula.fill=PatternFill(start_color="A9D08E",end_color="A9D08E",fill_type="solid")
+                celula.alignment=Alignment(horizontal="right")
+
+        dados=Reference(sheet,min_col=2,max_col=2,min_row=1,max_row=sheet.max_row-1)
+        grafico.title="Receitas Mensais"
+        
+    grafico.add_data(dados,titles_from_data=True)
+    grafico.set_categories(categorias)
+    grafico.y_axis.majorGridlines = None
+    sheet.add_chart(grafico,"G1")
+
+def criando_arquivo_excel(df_receitas_despesas,df_receitas_mensais,data_referencia):
     with tempfile.TemporaryDirectory() as dir_temp:
         nome_arquivo=f"Relatório mensal - {data_referencia}.xlsx"
         diretorio_arquivo_temporario= Path(dir_temp) / nome_arquivo
-
         with pd.ExcelWriter(diretorio_arquivo_temporario) as escritor:
             df_receitas_despesas.to_excel(escritor,sheet_name="Receitas e Despesas",index=False)
             df_receitas_mensais.to_excel(escritor,sheet_name="Receitas Mensais",index=False)
+            
+        wb=load_workbook(diretorio_arquivo_temporario)
+        sheet1=wb["Receitas e Despesas"]
+        sheet2=wb["Receitas Mensais"]
+        formatar_arquivo_excel(sheet1)
+        formatar_arquivo_excel(sheet2)
+        wb.save(diretorio_arquivo_temporario)
 
-        # wb=load_workbook(diretorio_arquivo_temporario)
         with open(diretorio_arquivo_temporario,"rb") as leitor:
             arquivo=leitor.read()
-            botao_download=st.sidebar.download_button("Clique para fazer o download do arquivo",
-                                                     data=arquivo,
-                                                     file_name=nome_arquivo,
-                                                     mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-    return arquivo
-
-
+            botao_download=st.sidebar.download_button("Clique para fazer o download",
+                                                    data=arquivo,
+                                                    file_name=nome_arquivo,
+                                                    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+    
 def main():
     upload_planilha=carregar_arquivo()
     if upload_planilha is not None:
@@ -129,9 +204,9 @@ def main():
         if colunas_selecionadas==True:
             df_formatado=formatar_colunas_dataframe(df,dict_colunas)
             df_filtrado,filtro_mes,data_referencia=filtrar_df_formatado(df_formatado)
-            df_receitas_despesas,df_receitas_mensais=gerar_dataframes_para_graficos(df_filtrado)
+            df_receitas_despesas,df_receitas_mensais=filtrar_dataframes_para_graficos(df_filtrado)
             gerar_graficos(df_receitas_despesas,df_receitas_mensais)
-            arquivo=formatar_para_excel(df_receitas_despesas,df_receitas_mensais,data_referencia)
+            criando_arquivo_excel(df_receitas_despesas,df_receitas_mensais,data_referencia)
     else:
         st.session_state.pop("colunas_selecionadas", None)
 
