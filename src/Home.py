9c7c94e4808c1523396568656a70bd69378cc418
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-04-30 17:02:57 -0300

    Atualizando Projeto

--- src/Home.py
+++ src/Home.py
@@ -2,7 +2,6 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 from pathlib import Path
-from datetime import datetime
 import tempfile
 from openpyxl import load_workbook
 from openpyxl.styles import Alignment,Font,Border,Side,PatternFill
@@ -15,60 +14,57 @@ def carregar_arquivo():
     upload_planilha=st.file_uploader("Selecione o arquivo:",accept_multiple_files=False,type=["xlsx","csv"])
     return upload_planilha
 
-def carregar_dataframe(arquivo) -> tuple[pd.DataFrame,str]:
+def carregar_dataframe(arquivo) -> pd.DataFrame:
     if arquivo.name.endswith("xlsx"):
         df=pd.read_excel(arquivo)
     if arquivo.name.endswith("csv"):
         df=pd.read_csv(arquivo)
     return df
 
-def selecionar_colunas_dataframe(df) -> tuple[dict[str,str], bool]:
+def selecionar_colunas_dataframe(df) -> dict[str,pd.DataFrame]:
     columns=st.sidebar
     columns.write("### Seleção de Colunas:")
     coluna_data=columns.selectbox("Selecione a coluna Data(dd/mm/yyyy)",list(df.columns),help="Coluna onde está a data da transação")
-    coluna_categoria=columns.selectbox("Selecione a coluna Categoria",[a for a in list(df.columns) if a != coluna_data],help="Coluna onde está a Categoria da transação")
-    coluna_tipo=columns.selectbox("Selecione a coluna Tipo(Receita/Despesa)",[a for a in list(df.columns) if a != coluna_data and a != coluna_categoria],help="Coluna onde está a tipo da transação")
-    coluna_valor=columns.selectbox("Selecione a coluna Valor",[a for a in list(df.columns) if a != coluna_data and a != coluna_categoria and a != coluna_tipo],help="Coluna onde está o valor da transação")
-    if "colunas_selecionadas" not in st.session_state:
-        st.session_state.colunas_selecionadas=False
-    if columns.button("Visualizar Dashboard"):
-        st.session_state.colunas_selecionadas=True
+    coluna_centro_custo=columns.selectbox("Selecione a coluna Centro de Custo",[a for a in list(df.columns) if a != coluna_data],help="Coluna onde está o centro de custo da transação")
+    coluna_valor=columns.selectbox("Selecione a coluna Valor",[a for a in list(df.columns) if a != coluna_data and a != coluna_centro_custo],help="Coluna onde está o valor da transação")
     dict_colunas={"Data":coluna_data,
-                  "Categoria":coluna_categoria, 
-                  "Tipo":coluna_tipo,
+                  "Centro de Custo":coluna_centro_custo, 
                   "Valor":coluna_valor}
-    return dict_colunas,st.session_state.colunas_selecionadas
-
+    return dict_colunas
+        
 def formatar_colunas_dataframe(df,colunas_dataframe) -> pd.DataFrame:
     df_formatado=df.copy()
     df_formatado=df_formatado.rename(columns={colunas_dataframe["Data"] :"Data",
-                                      colunas_dataframe["Categoria"] :"Categoria",
-                                      colunas_dataframe["Tipo"] : "Tipo",
+                                      colunas_dataframe["Centro de Custo"] :"Centro de Custo",
                                       colunas_dataframe["Valor"] : "Valor"
                                       })
-    formatacao_letras_tipo=df_formatado["Tipo"].str.lower()
-    coluna_formatada=pd.to_datetime(df_formatado["Data"],dayfirst=True,errors="coerce") #Formata a coluna data do DataFrame
-    df_formatado["Tipo"]=formatacao_letras_tipo
+    df_formatado=df_formatado[["Data","Centro de Custo","Valor"]]
+    valores_nulos=df_formatado["Centro de Custo"].fillna("Desconhecido")
+    coluna_formatada=pd.to_datetime(df_formatado["Data"],dayfirst=True,errors="coerce") 
+    df_formatado["Tipo"]=df_formatado["Valor"].apply(lambda x: "receita" if x > 0 else "despesa")
     df_formatado["Data"]=coluna_formatada
+    df_formatado["Centro de Custo"]=valores_nulos
+    df_formatado["Valor"]=df_formatado["Valor"].apply(abs)
     return df_formatado
 
-def filtrar_df_formatado(df_formatado) -> tuple[pd.DataFrame,str]:
+def filtrar_df_formatado_por_ano_mes(df_formatado) -> tuple[pd.DataFrame,str,str]:
     col1,col2=st.columns(2)
+    numero_para_meses={1:"Janeiro",2:"Fevereiro",3:"Março",4:"Abril",5:"Maio",6:"Junho",7:"Julho",8:"Agosto",9:"Setembro",10:"Outubro",11:"Novembro",12:"Dezembro"}
+    meses_para_numero={v:k for k, v in numero_para_meses.items() }
     ano=df_formatado["Data"].dt.year.unique()
-    dict_meses={1:"Janeiro",2:"Fevereiro",3:"Março",4:"Abril",5:"Maio",6:"Junho",7:"Julho",8:"Agosto",9:"Setembro",10:"Outubro",11:"Novembro",12:"Dezembro"}
     with col1:  
         filtro_ano=st.selectbox("Selecione o ano:",ano)
         meses_disponiveis=df_formatado.loc[df_formatado["Data"].dt.year == filtro_ano,"Data"].dt.month.unique()
     with col2:  
-        filtro_mes=st.selectbox("Selecione o mês:",[dict_meses[mes] for mes in meses_disponiveis])
+        filtro_mes=st.selectbox("Selecione o mês:",[numero_para_meses[mes] for mes in meses_disponiveis])
         df_filtrado=df_formatado.loc[(df_formatado["Data"].dt.year == filtro_ano) &
-                                 (df_formatado["Data"].dt.month == [k for k, v in dict_meses.items() if v == filtro_mes][0])]
+                                     (df_formatado["Data"].dt.month == meses_para_numero[filtro_mes])]
         data_filtrada=df_filtrado["Data"].iloc[0]
         data_referencia=data_filtrada.strftime("%Y-%m")
-    return df_filtrado,filtro_mes,data_referencia
+    return df_filtrado,filtro_mes,data_referencia 
 
-def filtrar_dataframes_para_graficos(df_filtrado) ->tuple[pd.DataFrame,pd.DataFrame]:
-    df_receitas_despesas=df_filtrado.pivot_table(index="Categoria",
+def filtrar_dataframes_para_graficos(df_filtrado) -> tuple[pd.DataFrame,pd.DataFrame]:
+    df_receitas_despesas=df_filtrado.pivot_table(index="Centro de Custo",
                                                        columns="Tipo",
                                                        values="Valor",
                                                        aggfunc="sum",
@@ -76,30 +72,26 @@ def filtrar_dataframes_para_graficos(df_filtrado) ->tuple[pd.DataFrame,pd.DataFr
                                                        )
     df_receitas_despesas=df_receitas_despesas.rename(columns={"receita":"Receitas",
                                                                 "despesa":"Despesas"}).reset_index()
-    df_receitas_despesas=df_receitas_despesas[["Categoria","Receitas","Despesas"]]
+    df_receitas_despesas=df_receitas_despesas[["Centro de Custo","Receitas","Despesas"]]
     df_receitas_despesas=df_receitas_despesas.sort_values(by="Receitas",ascending=False)
-    df_receitas_despesas.columns.name=None
-    df_receitas_despesas.loc[len(df_receitas_despesas)] = ["TOTAL",
-                                                           df_receitas_despesas["Receitas"].sum()
-                                                           ,df_receitas_despesas["Despesas"].sum()]
+    df_receitas_despesas.loc[len(df_receitas_despesas)] = ["TOTAL",df_receitas_despesas["Receitas"].sum(),df_receitas_despesas["Despesas"].sum()]
     receitas_mensais=df_filtrado.loc[df_filtrado["Tipo"]=="receita"]
-    df_receitas_mensais=receitas_mensais.groupby("Categoria")["Valor"].sum().sort_values(ascending=False).reset_index()
+    df_receitas_mensais=receitas_mensais.groupby("Centro de Custo")["Valor"].sum().sort_values(ascending=False).reset_index()
     df_receitas_mensais.loc[len(df_receitas_mensais)] = ["TOTAL",df_receitas_mensais["Valor"].sum()]
     return df_receitas_despesas,df_receitas_mensais
 
-def gerar_graficos(df_receitas_despesas,df_receitas_mensais) -> None:
+def gerar_graficos(df_receitas_despesas,df_receitas_mensais,filtro_mes) -> None:
     col1,col2=st.columns(2)
     with col1:    
         st.subheader("Total de Receitas e Despesas")
-        fig1=px.bar(df_receitas_despesas,x="Categoria",y=["Receitas","Despesas"],barmode="group",labels={"Categoria": "Categoria", "valor": "Valor"},title="Receitas e Despesas por Categoria")
+        fig1=px.bar(df_receitas_despesas,x="Centro de Custo",y=["Receitas","Despesas"],barmode="group",labels={"Categoria": "Categoria", "valor": "Valor"},title=f"Receitas e Despesas por Categoria em {filtro_mes}")
         fig1.update_layout(xaxis_tickangle=-45,xaxis_title="Categoria",yaxis_title="Valor",showlegend=True)
         col1.plotly_chart(fig1)
-    with col2:                  
+    with col2:               
         st.subheader("Categorias com maiores Receitas")
-        df_receitas_mensais_sem_total=df_receitas_mensais.loc[df_receitas_mensais["Categoria"]!="TOTAL"]
-        if len(df_receitas_mensais_sem_total)>2:
-            df_receitas_mensais_sem_total=df_receitas_mensais_sem_total.loc[0:2]
-        fig2=px.pie(df_receitas_mensais_sem_total,names="Categoria",values="Valor",title="Distribuição das maiores Receitas",color="Categoria")
+        if len(df_receitas_mensais)>2:
+            df_receitas_mensais=df_receitas_mensais.loc[0:4]
+        fig2=px.pie(df_receitas_mensais,names="Centro de Custo",values="Valor",title=f"Distribuição das maiores Receitas em {filtro_mes}",color="Centro de Custo")
         fig2.update_traces(textinfo="percent+label")       
         col2.plotly_chart(fig2)
     st.divider() 
@@ -191,24 +183,22 @@ def criando_arquivo_excel(df_receitas_despesas,df_receitas_mensais,data_referenc
 
         with open(diretorio_arquivo_temporario,"rb") as leitor:
             arquivo=leitor.read()
-            botao_download=st.sidebar.download_button("Clique para fazer o download",
-                                                    data=arquivo,
-                                                    file_name=nome_arquivo,
-                                                    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+            st.sidebar.download_button("Clique para fazer o download",
+                                        data=arquivo,
+                                        file_name=nome_arquivo,
+                                        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
     
 def main():
     upload_planilha=carregar_arquivo()
     if upload_planilha is not None:
         df=carregar_dataframe(upload_planilha)
-        dict_colunas,colunas_selecionadas=selecionar_colunas_dataframe(df)
-        if colunas_selecionadas==True:
-            df_formatado=formatar_colunas_dataframe(df,dict_colunas)
-            df_filtrado,filtro_mes,data_referencia=filtrar_df_formatado(df_formatado)
-            df_receitas_despesas,df_receitas_mensais=filtrar_dataframes_para_graficos(df_filtrado)
-            gerar_graficos(df_receitas_despesas,df_receitas_mensais)
-            criando_arquivo_excel(df_receitas_despesas,df_receitas_mensais,data_referencia)
-    else:
-        st.session_state.pop("colunas_selecionadas", None)
+        dict_colunas=selecionar_colunas_dataframe(df)
+        df_formatado=formatar_colunas_dataframe(df,dict_colunas)
+        df_filtrado,filtro_mes,data_referencia=filtrar_df_formatado_por_ano_mes(df_formatado)
+        df_receitas_despesas,df_receitas_mensais=filtrar_dataframes_para_graficos(df_filtrado)
+        gerar_graficos(df_receitas_despesas,df_receitas_mensais,filtro_mes)
+        criando_arquivo_excel(df_receitas_despesas,df_receitas_mensais,data_referencia)
+
 
 if __name__ == "__main__":
     main()
