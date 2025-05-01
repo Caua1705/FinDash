28eff757250db9af5048d587c2961644e767fa26
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-01 13:20:44 -0300

    Atualizando Projeto

--- src/Home.py
+++ src/Home.py
@@ -203,18 +203,11 @@ def main() -> None:
     if upload_planilha is not None:
         df=carregar_dataframe(upload_planilha)
         dict_colunas=selecionar_colunas_dataframe(df)
-        if "mostrar_dashboard" not in st.session_state:
-            st.session_state["mostrar_dashboard"]=False
-        if st.button("Visualizar Dashboard:"):
-            st.session_state["mostrar_dashboard"]=True
-        if st.session_state["mostrar_dashboard"]==True:
-                df_formatado=formatar_colunas_dataframe(df,dict_colunas)
-                df_filtrado,filtro_mes,data_referencia=filtrar_df_formatado_por_ano_mes(df_formatado)
-                df_receitas_despesas,df_receitas_mensais=filtrar_dataframes_para_graficos(df_filtrado)
-                gerar_graficos(df_receitas_despesas,df_receitas_mensais,filtro_mes)
-                criando_arquivo_excel(df_receitas_despesas,df_receitas_mensais,data_referencia)
-    else:
-        st.session_state.pop("mostrar_dashboard",None)
-
+        df_formatado=formatar_colunas_dataframe(df,dict_colunas)
+        df_filtrado,filtro_mes,data_referencia=filtrar_df_formatado_por_ano_mes(df_formatado)
+        df_receitas_despesas,df_receitas_mensais=filtrar_dataframes_para_graficos(df_filtrado)
+        gerar_graficos(df_receitas_despesas,df_receitas_mensais,filtro_mes)
+        criando_arquivo_excel(df_receitas_despesas,df_receitas_mensais,data_referencia)
+        
 if __name__ == "__main__":
     main()
