22818fc5cffa6234a6b3ece37b0bd65d15d6862c
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-02 20:31:01 -0300

    Update criacao_graficos.py

--- src/criacao_graficos.py
+++ src/criacao_graficos.py
@@ -27,11 +27,11 @@ def gerar_graficos(df_receitas_despesas,df_receitas_mensais,filtro_mes) -> None:
     col1,col2=st.columns(2)
     with col1:    
         st.subheader("Total de Receitas e Despesas")
-        fig1=px.bar(df_receitas_despesas,x="Centro de Custo",y=["Receitas","Despesas"],barmode="group",labels={"Categoria": "Categoria", "valor": "Valor"},title=f"Receitas e Despesas por Categoria em {filtro_mes}")
+        fig1=px.bar(df_receitas_despesas,x="Centro de Custo",y=["Receitas","Despesas"],barmode="group",labels={"Categoria": "Categoria", "valor": "Valor"},title=f"Receitas e Despesas por Centro de Custo em {filtro_mes}")
         fig1.update_layout(xaxis_tickangle=-45,xaxis_title="Categoria",yaxis_title="Valor",showlegend=True)
         col1.plotly_chart(fig1)
     with col2:               
-        st.subheader("Categorias com maiores Receitas")
+        st.subheader("Centros de Receita com maiores receitas")
         if len(df_receitas_mensais)>2:
             df_receitas_mensais=df_receitas_mensais.loc[0:2]
         fig2=px.pie(df_receitas_mensais,names="Centro de Custo",values="Valor",title=f"Distribuição das maiores Receitas em {filtro_mes}",color="Centro de Custo")
