560b465656a99cfc7d72a36b01d9bfc95689b998
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-05 14:37:02 -0300

    Update criacao_graficos.py

--- src/analise_clientes/criacao_graficos.py
+++ src/analise_clientes/criacao_graficos.py
@@ -9,21 +9,28 @@ def filtrar_dataframes_para_graficos(df_filtrado_para_grafico):
                                         index="Cliente / Fornecedor",
                                         aggfunc="sum").reset_index()
     df_gerado=df_gerado.loc[df_gerado["Cliente / Fornecedor"]!="Desconhecido"]
+    if (coluna for coluna in df_gerado.columns == "Receitas"):
+         df_gerado=df_gerado.sort_values(by="Receitas",ascending=False)
+    else:
+        df_gerado=df_gerado.sort_values(by="Despesas",ascending=False)
     return df_gerado
 
 def gerar_graficos(df_filtrado_clientes,df_filtrado_fornecedores):
-        df_filtrado_clientes=df_filtrado_clientes.sort_values(by="Receitas",ascending=False)
         if len(df_filtrado_clientes)>10:
             df_filtrado_clientes=df_filtrado_clientes.iloc[:10]
         st.markdown("### Principais Clientes:")
         fig1=px.bar(df_filtrado_clientes,x="Receitas",y="Cliente / Fornecedor",title="Participação dos principais clientes nas receitas",color="Cliente / Fornecedor",orientation="h")
         fig1.update_layout(xaxis_title="Receitas",yaxis_title="Clientes",showlegend=False,height=370,bargap=0.3)
         st.plotly_chart(fig1,use_container_width=True)
-        df_filtrado_fornecedores=df_filtrado_fornecedores.sort_values(by="Despesas",ascending=False)
+        st.write(df_filtrado_clientes)
+
         if len(df_filtrado_fornecedores)>5:
             df_filtrado_fornecedores=df_filtrado_fornecedores.iloc[:10]
         st.markdown("### Principais Fornecedores:")
         fig2=px.bar(df_filtrado_fornecedores,x="Despesas",y="Cliente / Fornecedor",title="Participação dos principais fornecedores nas despesas",color="Cliente / Fornecedor",orientation="h")
         fig2.update_layout(xaxis_title="Despesas",yaxis_title="Fornecedores",showlegend=False,height=370,bargap=0.3)
         st.plotly_chart(fig2,use_container_width=True)
-        
+        st.write(df_filtrado_fornecedores)
+
+def criar_metricas():
+      pass
