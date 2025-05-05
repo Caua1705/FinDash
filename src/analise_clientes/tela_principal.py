13cfee5d9f523dd537a70135a9eb923678243139
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-05 16:33:00 -0300

    Update tela_principal.py

--- src/analise_clientes/tela_principal.py
+++ src/analise_clientes/tela_principal.py
@@ -19,16 +19,16 @@ def criar_metricas(df_filtrado_clientes,df_filtrado_fornecedores,df_clientes):
     col1,col2,col3,col4=st.columns(4)
     #Top 1º Cliente:
     with col1:
-      st.metric("Top 1 Cliente",df_filtrado_clientes.iloc[0,0], f"R$ {df_filtrado_clientes.iloc[0,1]:.2f}")
-    with col2:
-      ticket_medio_cliente=df_filtrado_clientes["Receitas"].sum() / len(df_filtrado_clientes)
-      st.metric("Ticket Médio por Cliente",f"R$ {ticket_medio_cliente}")
-    with col3:
-      st.metric("Principal Fornecedor",df_filtrado_fornecedores.iloc[0,0], f"R$ {df_filtrado_fornecedores.iloc[0,1]:.2f}")
-    with col4:
       df_clientes=df_clientes.loc[df_clientes["Cliente / Fornecedor"]!="Desconhecido","Cliente / Fornecedor"]
       clientes_com_mais_transacoes=df_clientes.value_counts().reset_index()
-      st.metric("Cliente com mais transações",clientes_com_mais_transacoes.iloc[0,0],f"{clientes_com_mais_transacoes.iloc[0,1]} transações")
+      st.metric("🧾 Cliente com mais transações",clientes_com_mais_transacoes.iloc[0,0],f"{clientes_com_mais_transacoes.iloc[0,1]} transações")
+    with col2:
+      st.metric("💰 Top 1 Cliente",df_filtrado_clientes.iloc[0,0], f"R$ {df_filtrado_clientes.iloc[0,1]:.2f}")
+    with col3:
+      ticket_medio_cliente=df_filtrado_clientes["Receitas"].sum() / len(df_filtrado_clientes)
+      st.metric("🎟️ Ticket Médio por Cliente",f"R$ {ticket_medio_cliente:.2f}")
+    with col4:
+      st.metric("🏢 Principal Fornecedor",df_filtrado_fornecedores.iloc[0,0], f"R$ {df_filtrado_fornecedores.iloc[0,1]:.2f}")
     st.divider()
 
 
