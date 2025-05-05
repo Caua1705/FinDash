e039c8bd27d73d19ec828e6456aa4d04b3292a87
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-05 13:04:12 -0300

    Update criacao_graficos.py

--- src/analise_clientes/criacao_graficos.py
+++ src/analise_clientes/criacao_graficos.py
@@ -24,9 +24,9 @@ def gerar_graficos(df_filtrado_clientes,df_filtrado_fornecedores):
     with col2:
         df_filtrado_fornecedores=df_filtrado_fornecedores.sort_values(by="Despesas",ascending=False)
         if len(df_filtrado_fornecedores)>5:
-            df_filtrado_fornecedores=df_filtrado_fornecedores.iloc[:5]
+            df_filtrado_fornecedores=df_filtrado_fornecedores.iloc[:10]
         st.subheader("Distribuição de Despesas por Fornecedor")
-        fig1=px.bar(df_filtrado_fornecedores,x="Cliente / Fornecedor",y="Despesas",title="Participação dos principais fornecedores nas despesas",color="Cliente / Fornecedor",orientation="h")
+        fig1=px.bar(df_filtrado_fornecedores,x="Despesas",y="Cliente / Fornecedor",title="Participação dos principais fornecedores nas despesas",color="Cliente / Fornecedor",orientation="h")
         fig1.update_layout(xaxis_title="Fornecedores",yaxis_title="Despesas",showlegend=False,height=370,bargrap=0.3)
         col2.plotly_chart(fig1)
         
