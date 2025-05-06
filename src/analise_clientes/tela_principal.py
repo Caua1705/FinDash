86a6a760bf5c6390377575e54fd58bf13bf43068
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-05 21:54:31 -0300

    Update tela_principal.py

--- src/analise_clientes/tela_principal.py
+++ src/analise_clientes/tela_principal.py
@@ -35,12 +35,14 @@ def gerar_graficos(df_filtrado_clientes,df_filtrado_fornecedores):
   with col1:
     if len(df_filtrado_clientes)>10:
         df_filtrado_clientes=df_filtrado_clientes.iloc[:10]
+    st.markdown("### 📊 Principais Clientes")
     fig1=px.bar(df_filtrado_clientes,x="Receitas",y="Cliente / Fornecedor",title="Participação dos principais clientes nas receitas",color="Cliente / Fornecedor",orientation="h")
     fig1.update_layout(xaxis_title="Receitas",yaxis_title="Clientes",showlegend=False,height=370,bargap=0.3)
     st.plotly_chart(fig1,use_container_width=True)
   with col2:
     if len(df_filtrado_fornecedores)>10:
         df_filtrado_fornecedores=df_filtrado_fornecedores.iloc[:10]
+    st.markdown("### 📉 Principais Fornecedores:")
     fig2=px.bar(df_filtrado_fornecedores,x="Despesas",y="Cliente / Fornecedor",title="Participação dos principais fornecedores nas despesas",color="Cliente / Fornecedor",orientation="h")
     fig2.update_layout(xaxis_title="Despesas",yaxis_title="Fornecedores",showlegend=False,height=370,bargap=0.3)
     st.plotly_chart(fig2,use_container_width=True)
