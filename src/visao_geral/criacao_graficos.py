c655012bccea4b21a69077b160f8989f8a8f2d72
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-06 19:57:38 -0300

    Update criacao_graficos.py

--- src/visao_geral/criacao_graficos.py
+++ src/visao_geral/criacao_graficos.py
@@ -47,28 +47,32 @@ def criacao_metricas(df_receitas_despesas,df_receitas_despesas_anterior,filtro_m
         total_receitas_anterior=df_receitas_despesas_anterior["Receitas"].sum()
         if total_receitas_anterior==0:
             delta_receitas=0
-        delta_receitas=(total_receitas-total_receitas_anterior) / total_receitas_anterior * 100
+        else:
+            delta_receitas=(total_receitas-total_receitas_anterior) / total_receitas_anterior * 100
         st.metric("Receita Total",f"R$ {total_receitas:,.2f}",f"{delta_receitas:.2f}%",delta_color="normal")
     with col2:
         total_despesas=df_receitas_despesas["Despesas"].sum()
         total_despesas_anterior=df_receitas_despesas_anterior["Despesas"].sum()
         if total_despesas_anterior==0:
             delta_despesas=0
-        delta_despesas=(total_despesas_anterior-total_despesas) / total_despesas_anterior * 100
+        else:
+            delta_despesas=(total_despesas_anterior-total_despesas) / total_despesas_anterior * 100
         st.metric("Despesa Total",f"R$ {total_despesas:,.2f}",f"{delta_despesas:.2f}%",delta_color="inverse")
     with col3:
         saldo_liquido=total_receitas-total_despesas
         saldo_liquido_anterior=total_receitas_anterior-total_despesas_anterior
         if saldo_liquido_anterior==0:
             delta_saldo=0
-        delta_saldo=(saldo_liquido-saldo_liquido_anterior) / saldo_liquido_anterior * 100
+        else:
+            delta_saldo=(saldo_liquido-saldo_liquido_anterior) / saldo_liquido_anterior * 100
         st.metric("Saldo Líquido",f"R$ {saldo_liquido:,.2f}",f"{delta_saldo:.2f}%",delta_color="normal")
     with col4:
         roi_consolidado=(saldo_liquido/total_despesas) * 100
         roi_consolidado_anterior=(saldo_liquido_anterior/total_despesas_anterior) * 100
         if roi_consolidado_anterior==0:
             delta_roi=0
-        delta_roi= (roi_consolidado - roi_consolidado_anterior) / roi_consolidado_anterior * 100
+        else:
+            delta_roi= (roi_consolidado - roi_consolidado_anterior) / roi_consolidado_anterior * 100
         st.metric("ROI Consolidado",f"{roi_consolidado:,.2f}%",f"{delta_roi:.2f}%")
     st.divider()
 
