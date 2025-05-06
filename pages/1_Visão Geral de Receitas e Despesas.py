dee70b6a3befad507bad4981df46a08e7ed2dec4
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-06 19:35:09 -0300

    Update 1_Visão Geral de Receitas e Despesas.py

--- pages/1_Visão Geral de Receitas e Despesas.py
+++ pages/1_Visão Geral de Receitas e Despesas.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from src.visao_geral.criacao_graficos import agrupar_df_filtrado_para_grafico_receita,agrupar_df_filtrado_para_metricas, gerar_graficos, filtrar_por_ano_mes, criacao_metricas ,grafico_evolucao 
+from src.visao_geral.criacao_graficos import agrupar_df_filtrado_para_grafico_receita,agrupar_df_filtrado_para_metricas, gerar_graficos, filtrar_por_ano_mes, criacao_metricas  
 from src.visao_geral.criacao_arquivo_excel import criando_arquivo_excel
 def main() -> None:
     if "df_formatado" in st.session_state:
@@ -9,21 +9,21 @@ def main() -> None:
 
         tabs = st.tabs(["Resumo do Mês", "Evolução Mensal"])
         
-        with tabs[0]:
+        # with tabs[0]:
 
-            df_filtrado,df_filtrado_anterior,filtro_mes,data_referencia,numero_para_meses=filtrar_por_ano_mes(df_formatado)
+        df_filtrado,df_filtrado_anterior,filtro_mes,data_referencia,numero_para_meses=filtrar_por_ano_mes(df_formatado)
     
-            df_receitas_despesas=agrupar_df_filtrado_para_metricas(df_filtrado,filtro_mes)
-            df_receitas_despesas_anterior=agrupar_df_filtrado_para_metricas(df_filtrado_anterior,filtro_mes)
+        df_receitas_despesas=agrupar_df_filtrado_para_metricas(df_filtrado,filtro_mes)
+        df_receitas_despesas_anterior=agrupar_df_filtrado_para_metricas(df_filtrado_anterior,filtro_mes)
 
-            criacao_metricas(df_receitas_despesas,df_receitas_despesas_anterior,filtro_mes)
+        criacao_metricas(df_receitas_despesas,df_receitas_despesas_anterior,filtro_mes)
 
-            df_receitas_mensais=agrupar_df_filtrado_para_grafico_receita(df_filtrado)
+        df_receitas_mensais=agrupar_df_filtrado_para_grafico_receita(df_filtrado)
 
-            gerar_graficos(df_receitas_despesas,df_receitas_mensais,filtro_mes) 
-            criando_arquivo_excel(df_receitas_despesas,df_receitas_mensais,data_referencia)
-        with tabs[1]:
-            grafico_evolucao(df_formatado,numero_para_meses) 
+        gerar_graficos(df_receitas_despesas,df_receitas_mensais,filtro_mes) 
+        criando_arquivo_excel(df_receitas_despesas,df_receitas_mensais,data_referencia)
+        # with tabs[1]:
+        #     grafico_evolucao(df_formatado,numero_para_meses) 
 
     else:
         st.warning("Faça o upload do arquivo!")
