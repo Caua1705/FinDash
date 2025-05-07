2bfd9591dd058935d1279d7458c1ce35862e0bbf
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-07 14:18:55 -0300

    Update transacoes.py

--- analise_clientes/transaçoes_detalhadas/transacoes.py
+++ analise_clientes/transaçoes_detalhadas/transacoes.py
@@ -5,6 +5,6 @@ def exibir_transacoes(df_filtrado):
                                                       "Cliente / Fornecedor"].unique()
   valor_escolhido=st.selectbox("Selecione um Cliente ou Fornecedor",valores_pesquisa)
   df_valor_escolhido=df_filtrado.loc[df_filtrado["Cliente / Fornecedor"]==valor_escolhido]
-  df_valor_escolhido=df_valor_escolhido.drop("Mês")
+  df_valor_escolhido=df_valor_escolhido.drop(columns=["Mês"])
   st.write(df_valor_escolhido)
   st.success(f"💰 Total das Transações: R$ {df_valor_escolhido['Valor'].sum():,.2f}")
